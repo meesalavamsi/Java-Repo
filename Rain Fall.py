59d14dd8b1dc1cba93ad46e9f918bb19170042c1
meesalavamsi <156990795+meesalavamsi@users.noreply.github.com>
2024-10-18 00:36:39 +0530

    Uploading code: Rain Fall.py

---
+++ Rain Fall.py
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        if(x<3)
+        {
+            System.out.println("LIGHT");
+        }
+        else if(x>=3 && x<7)
+        {
+            System.out.println("MODERATE");
+        }
+        else{
+            System.out.println("HEAVY");
+        }
+    }
+}
